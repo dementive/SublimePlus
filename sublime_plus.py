521ec426ba6b8d115d581238e4f343ae73c408c2
dementive <87823030+dementive@users.noreply.github.com>
2025-07-12 15:31:13 -0500

    Minimap setting

--- sublime_plus.py
+++ sublime_plus.py
@@ -1128,6 +1128,17 @@ class FileHistoryInputHandler(sublime_plugin.ListInputHandler):
         return files
 
 
+class MinimapSetting(sublime_plugin.EventListener):
+    def on_activated(self, view):
+        show_minimap = view.settings().get('show_minimap')
+        if show_minimap:
+            view.window().set_minimap_visible(True)
+        elif show_minimap is not None:
+            view.window().set_minimap_visible(False)
+        else:
+            view.window().set_minimap_visible(True)
+
+
 class GotoRecentListener(sublime_plugin.EventListener):
     def on_deactivated(self, view):
         if view.file_name():
